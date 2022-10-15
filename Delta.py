d5a0f79def179ee5417a52f391380ac6125dc989
M4recki <111280515+M4recki@users.noreply.github.com>
2022-10-15 11:39:47 +0200

    Add files via upload

--- Delta.py
+++ Delta.py
@@ -1,25 +1,50 @@
 import math
+
+# Data input
 a = float(input("Enter a: "))
 b = float(input("Enter b: "))
 c = float(input("Enter c: "))
-char = str(input("Enter majority, minority, or equal character: "))
-#Data input
-delta = b**2+(-4)*a*c
-#Delta calculation
-print("Delta is: ", delta)
-if delta>0:
-    deltaSqrt = math.sqrt(delta)
-    #Square root of delta
-    print("The root of the delta is:", deltaSqrt)
-    x1=((-b)-deltaSqrt)/2*a
-    print("x1 is: ", x1)
-    x2=((-b)+deltaSqrt)/2*a
-    print("x2 is: ", x2)
-    #Find square root of delta then calculate x1 and x2: Variant when root of delta is positive
-elif delta==0:
-    x0=(-b)/2*a
-    print("x0 is: ", x0)
-    #Variant when root of delta is 0
+
+# Mathematical operations
+
+
+def delta(x, y, z):
+    return y**2+(-4)*x*z
+# ▲ = b²*(-4)*a*c
+
+
+def deltaSqrt():
+    return math.sqrt(delta(a, b, c))
+# √▲
+
+
+def x1(x, y):
+    return ((-y)-deltaSqrt())/(2*x)
+# x1 = (-b-√▲)/2*a
+
+
+def x2(x, y):
+    return ((-y)+deltaSqrt())/(2*x)
+# x2 = (-b+√▲)/2*a
+
+
+def x0(x, y):
+    return (-y)/(2*x)
+#x0 = -b/2*a
+
+
+print("Delta is: ", delta(a, b, c))
+if delta(a, b, c) > 0 or delta(a, b, c) == 0:
+    print("Square of the delta is: ", deltaSqrt())
 else:
-    print("x !=0. No solutions.")
-    #Variant when root of delta is negative
+    print("There is no square root of a negative number")
+# Presentation of the delta root results
+
+if delta(a, b, c) > 0:
+    print("x1 is: ", x1(a, b))
+    print("x2 is: ", x2(a, b))
+elif delta(a, b, c) == 0:
+    print("x0 is: ", x0(a, b))
+else:
+    print(delta(a, b, c), "is < 0. No solutions")
+# Showing the results of the calculations x1 x2 or x0 depending on the results from the previous lines of the program
