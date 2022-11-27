bdd68b136a670498ed6c872bcda1547203b704fd
M4recki <111280515+M4recki@users.noreply.github.com>
2022-11-27 16:10:25 +0100

    Add files via upload

---
+++ BlackJack.py
@@ -0,0 +1,156 @@
+import random
+from replit import clear
+
+Cards = {"Jack": 10, "Queen": 10, "King": 10, "Ace": 11, "2": 2,
+         "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10}
+
+DrawedCardsByUser = []
+DrawedCardsByComputer = []
+MaxValue = 21
+
+
+def CardDrawForUser():
+    Values = list(Cards.values())
+    UsersCard1 = random.choice(Values)
+    UsersCard2 = random.choice(Values)
+    DrawedCardsByUser.append(UsersCard1)
+    DrawedCardsByUser.append(UsersCard2)
+    return UsersCard1, UsersCard2
+
+def CardDrawForComputer():
+    Values = list(Cards.values())
+    ComputersCard1 = random.choice(Values)
+    ComputersCard2 = random.choice(Values)
+    DrawedCardsByComputer.append(ComputersCard1)
+    DrawedCardsByComputer.append(ComputersCard2)
+    return ComputersCard1, ComputersCard2
+
+def NewCardForUser():
+    Values = list(Cards.values())
+    UsersCard3 = random.choice(Values)
+    DrawedCardsByUser.append(UsersCard3)
+    return UsersCard3
+
+def NewCardForComputer():
+    Values = list(Cards.values())
+    ComputersCard3 = random.choice(Values)
+    DrawedCardsByComputer.append(ComputersCard3)
+    return ComputersCard3
+
+def Check():
+    if sum(DrawedCardsByUser) > MaxValue:
+        print("You went over. You lose.")
+        PlayAgain = input("Do you want to play a game of Blackjack? Type 'y' - yes or 'n' - no: ")
+        if PlayAgain == "y":
+            ClearEverything()
+            Game()
+        elif PlayAgain == "n":
+            ClearEverything()
+            quit()
+        else:
+            print("Invalid input. Please try again.")
+            quit()
+    elif sum(DrawedCardsByComputer) > MaxValue:
+        print("Computer went over. You won.")
+        PlayAgain = input("Do you want to play a game of Blackjack? Type 'y' - yes or 'n' - no: ")
+        if PlayAgain == "y":
+            ClearEverything()
+            Game()
+        elif PlayAgain == "n":
+            ClearEverything()
+            quit()
+        else:
+            print("Invalid input. Please try again.")
+            quit()
+    elif sum(DrawedCardsByUser) > sum(DrawedCardsByComputer):
+        print("You won.")
+        PlayAgain = input("Do you want to play a game of Blackjack? Type 'y' - yes or 'n' - no: ")
+        if PlayAgain == "y":
+            ClearEverything()
+            Game()
+        elif PlayAgain == "n":
+            ClearEverything()
+            quit()
+        else:
+            print("Invalid input. Please try again.")
+            quit()
+    elif sum(DrawedCardsByUser) < sum(DrawedCardsByComputer):
+        print("You lost")
+        PlayAgain = input("Do you want to play a game of Blackjack? Type 'y' - yes or 'n' - no: ")
+        if PlayAgain == "y":
+            ClearEverything()
+            Game()
+        elif PlayAgain == "n":
+            ClearEverything()
+            quit()
+        else:
+            print("Invalid input. Please try again.")
+            quit()
+    elif sum(DrawedCardsByUser) == sum(DrawedCardsByComputer):
+        print("It's a draw!")
+        PlayAgain = input("Do you want to play a game of Blackjack? Type 'y' - yes or 'n' - no: ")
+        if PlayAgain == "y":
+            ClearEverything()
+            Game()
+        elif PlayAgain == "n":
+            ClearEverything()
+            quit()
+        else:
+            print("Invalid input. Please try again.")
+            quit()
+
+def FinalCards():
+    print("Your final cards:", DrawedCardsByUser, "final score:", sum(DrawedCardsByUser))
+    print()
+    print("Computer's final cards:", DrawedCardsByComputer, "final score:", sum(DrawedCardsByComputer))
+    print()
+    Check()
+
+def ClearEverything():
+    clear()
+    DrawedCardsByUser.clear()
+    DrawedCardsByComputer.clear()
+
+def Game():
+    while True:
+
+        Start = input("Welcome to the Blackjack game. Would you like to play? 'y' - yes 'n' - no: ")
+        if Start == "y":
+            CardDrawForUser()
+            print("Your cards:", DrawedCardsByUser, "current score:", sum(DrawedCardsByUser))
+            print()
+            CardDrawForComputer()
+            print("Computer's 1st card:", DrawedCardsByComputer[0])
+            print()
+
+            while sum(DrawedCardsByUser) <= MaxValue or sum(DrawedCardsByComputer) <= MaxValue:
+                NewCard = input("Type 'y' to get another card, type 'n' to pass: ")
+                if NewCard == "y":
+                    NewCardForUser()
+                    NewCardForComputer()
+                    print("Your cards:", DrawedCardsByUser, "current score:", sum(DrawedCardsByUser))
+                    print()
+                    print("Computer's 1st card:", DrawedCardsByComputer[0])
+                    print()
+                    if sum(DrawedCardsByUser) > MaxValue: 
+                        FinalCards()
+                        Check()
+                    elif sum(DrawedCardsByUser) == sum(DrawedCardsByComputer):
+                        FinalCards()
+                        Check()
+                    else:
+                        continue
+                elif NewCard == "n":
+                    FinalCards()
+                else:
+                    print("Invalid input. Please try again.")
+                    continue
+
+        elif Start == "n":
+            ClearEverything()
+            break
+        else:
+            print("Invalid input. Please try again.")
+            continue
+
+Game()
